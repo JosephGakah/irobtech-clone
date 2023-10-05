2c0deb136ef2e011408397797e58b9c6ae9fca52
JosephGakah <joegakah@gmail.com>
2023-10-05 23:07:46 +0300

    Projects

--- src/app/our-team/page.tsx
+++ src/app/our-team/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import React from 'react'
 import TeamMember from './components/team'
 import { Team } from '@/lib/team'
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Projects(){
+    return(
+        <section className='bg-[#060a23] py-[125px] px-[35px]'>
+            <div className=''>
+                <div className='mx-[10%] pt-[100px] h-[45vh]'>
+                    <div className='mb-[10px] text-center'>
+                        <h2 className='text-white text-[50px] font-[400]'>Projects <span className='font-[800]'>Recently &nbsp;Completed</span></h2>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
