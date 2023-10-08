0be20815ecf0b6f487a0d448178bcb81fa1820c4
JosephGakah <joegakah@gmail.com>
2023-10-08 22:47:14 +0300

    Footer section 1

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Footer() {
@@ -5,9 +6,21 @@ export default function Footer() {
     <footer>
         <section>
             <div className='max-w-[1140px] m-auto'>
-                <div className='flex justify-center py-[10px] px-[15x]'>
-                    <div className='w-[65%]'></div>
-                    <div className='w-[35%]'></div>
+                <div className='flex justify-center py-[10px] px-[15x] bg-white rounded-[10px]'>
+                    <div className='flex flex-wrap w-[65%]'>
+                        <div className='py-[20px] px-[40px]'>
+                            <p className="w-[80%] text-[#060515] text-[22px] font-[400]">
+                                We solve your problems with amazing latest technologies
+                            </p>
+                        </div>
+                    </div>
+                    <div className='w-[35%]'>
+                        <div className='py-[20px] px-[40px] flex justify-end uppercase'>
+                            <Link className='button bg-[#01106d] text-[13px] py-[18px] px-[34px] rounded-[25px] text-white' href={''}>
+                                Get In Touch
+                            </Link>
+                        </div>
+                    </div>
                 </div>
             </div>
 
