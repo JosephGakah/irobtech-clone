461990c8cf9000fa0fe45c787a103ff48b3593ea
JosephGakah <joegakah@gmail.com>
2023-10-08 22:18:16 +0300

    Footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+        <section>
+            <div className='max-w-[1140px] m-auto'>
+                <div className='flex justify-center py-[10px] px-[15x]'>
+                    <div className='w-[65%]'></div>
+                    <div className='w-[35%]'></div>
+                </div>
+            </div>
+
+        </section>
+        <section>
+
+        </section>
+    </footer>
+  )
+}
